a87eb3b01b629bf91285d1b53ce64e4a9ca25bab
NissrineELMNIAI <nissrineelmniai0@gmail.com>
2026-04-08 14:17:42 +0100

    API REST Quiz

---
+++ packages/smartest-backend/src/main/java/com/smartest/backend/dto/response/QuizResponse.java
@@ -0,0 +1,21 @@
+package com.smartest.backend.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuizResponse {
+
+    private Long id;
+    private String titre;
+    private Integer duree;
+    private Long professeurId;
+    private String professeurNom;
+    private Long coursId;
+    private String coursTitre;
+    private List<QuestionResponse> questions;
+}
