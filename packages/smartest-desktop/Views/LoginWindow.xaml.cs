3c0ab4f99c8033b08ca873c8ee8eb843c977f5a5
oumeyma-elaammari <oumeyma.elaammari.ensao@ump.ac.ma>
2026-04-20 20:33:04 +0100

    message

--- packages/smartest-desktop/Views/LoginWindow.xaml.cs
+++ packages/smartest-desktop/Views/LoginWindow.xaml.cs
@@ -12,11 +12,30 @@ namespace smartest_desktop.Views
             DataContext = new LoginViewModel();
         }
 
-        private void PasswordBox_PasswordChanged(
-            object sender, RoutedEventArgs e)
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
             if (DataContext is LoginViewModel vm)
                 vm.Password = ((PasswordBox)sender).Password;
+        }
+
+        private void TogglePassword_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox.Visibility == Visibility.Visible)
+            {
+                // Passer en mode texte clair
+                PasswordTextBox.Text = PasswordBox.Password;
+                PasswordBox.Visibility = Visibility.Collapsed;
+                PasswordTextBox.Visibility = Visibility.Visible;
+                EyeIcon.Text = "🔒";
+            }
+            else
+            {
+                // Repasser en mode masqué
+                PasswordBox.Password = PasswordTextBox.Text;
+                PasswordTextBox.Visibility = Visibility.Collapsed;
+                PasswordBox.Visibility = Visibility.Visible;
+                EyeIcon.Text = "🔓";
+            }
         }
     }
 }
